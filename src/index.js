86c04f9e5d828457084d123592fd6cd3f05cc509
matthewjsloane <msloane@thoughtworks.com>
2016-06-23 11:16:23 -0700

    [Matthew] Uses ES6 syntax for readability.

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-var APP_ID = undefined; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
-var AlexaSkill = require('./AlexaSkill');
+const APP_ID = undefined; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+const AlexaSkill = require('./AlexaSkill');
 
-var Fli = function () {
+const Fli = function() {
     AlexaSkill.call(this, APP_ID);
 };
 
 Fli.prototype = Object.create(AlexaSkill.prototype);
 Fli.prototype.constructor = Fli;
 
-Fli.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-    var commonText = "To add an event, say 'add event, event type for story story ID.";
+Fli.prototype.eventHandlers.onLaunch = (launchRequest, session, response) => {
+    var commonText = "To add an event, say 'add event event-type for story story ID.";
     var repromptText = "Remember, " + commonText;
     var promptText = "Welcome to Fly. " + commonText;
     response.ask(promptText, repromptText);
 };
 
 Fli.prototype.intentHandlers = {
-    "FliIntent": function (intent, session, response) {
-        var http = require('http');
-        var moment = require('moment');
+    "FliIntent": (intent, session, response) => {
+        const http = require('http');
+        const moment = require('moment');
 
-        var postData = JSON.stringify({
+        const postData = JSON.stringify({
             eventType: intent.slots.Event.value.replace(/ /g,"_"),
             occurredAt: moment.utc().format('YYYY-MM-DD HH:mm:ss'),
             story: intent.slots.Number.value
         });
 
-        var options = {
+        const options = {
             hostname: 'fli-change.herokuapp.com',
             port: 80,
             path: '/events',
@@ -37,7 +37,7 @@ Fli.prototype.intentHandlers = {
           }
         };
 
-        var req = http.request(options, function(res) {
+        const req = http.request(options, (res) => {
           if (res.statusCode === 409) {
             response.tell("Sorry, your event already exists.");
           } else {
@@ -45,21 +45,21 @@ Fli.prototype.intentHandlers = {
           }
         });
 
-        req.on('error', function(e) {
+        req.on('error', (e) => {
             response.tell("Sorry, I was unable to add your event.");
         });
 
         req.write(postData);
         req.end();
     },
-    "AMAZON.HelpIntent": function (intent, session, response) {
+    "AMAZON.HelpIntent": (intent, session, response) => {
         response.ask("Event types are words such as start and end, and story IDs are numbers." +
                      "A sample input is as follows: 'add event start for story 19'.");
     }
 };
 
 exports.handler = function (event, context) {
-    var fli = new Fli();
+    const fli = new Fli();
     fli.execute(event, context);
 };
 
