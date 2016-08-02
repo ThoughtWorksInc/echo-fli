cf40d14932513befe504508cc681ba334b9685cf
Addison Lee <addison.v.lee@gmail.com>
2016-08-02 14:36:26 -0700

    Add smoke test for deployed lambda

---
+++ test/smoke.js
@@ -0,0 +1,22 @@
+const expect = require('chai').expect;
+const AWS = require('aws-sdk');
+
+it('Lambda should be successfully deployed in AWS', function (done) {
+  const lambda = new AWS.Lambda({region: 'us-east-1'});
+  const params = {
+    FunctionName: require('../package.json').name,
+    Payload: `{
+        "session": {},
+        "version": "1.0",
+        "request": { "type": "LaunchRequest" }
+      }`
+  };
+
+  lambda.invoke(params, function (err, data) {
+    expect(err).to.eql(null);
+    expect(data.StatusCode).to.equal(200);
+    expect(data.FunctionError).to.be.undefined;
+    expect(JSON.parse(data.Payload).response.outputSpeech.text).to.equal('Let\'s fly!');
+    done();
+  });
+});
