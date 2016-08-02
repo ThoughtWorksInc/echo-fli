a3c70a2c9a22232d6864b590f7eab097634cc213
Addison Lee <addison.v.lee@gmail.com>
2016-08-02 15:05:33 -0700

    Code cleanup using eslint and editorconfig

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 6
+  },
+  "env": {
+    "node": true,
+    "mocha": true
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
--- functions/index.js
+++ functions/index.js
@@ -1,63 +1,63 @@
 const APP_ID = undefined; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
 const AlexaSkill = require('./AlexaSkill');
 
-const Fli = function() {
-    AlexaSkill.call(this, APP_ID);
+const Fli = function () {
+  AlexaSkill.call(this, APP_ID);
 };
 
 Fli.prototype = Object.create(AlexaSkill.prototype);
 Fli.prototype.constructor = Fli;
 
 Fli.prototype.eventHandlers.onLaunch = (launchRequest, session, response) => {
-    var repromptText = "Remember, to add an event, say 'add event, event-type, for story, story ID.";
-    var promptText = "Let's fly!";
-    response.ask(promptText, repromptText);
+  var repromptText = 'Remember, to add an event, say \'add event, event-type, for story, story ID.';
+  var promptText = 'Let\'s fly!';
+  response.ask(promptText, repromptText);
 };
 
 Fli.prototype.intentHandlers = {
-    "FliIntent": (intent, session, response) => {
-        const http = require('http');
-        const moment = require('moment');
+  'FliIntent': (intent, session, response) => {
+    const http = require('http');
+    const moment = require('moment');
 
-        const postData = JSON.stringify({
-            eventType: intent.slots.Event.value.replace(/ /g,"_"),
-            occurredAt: moment.utc().format('YYYY-MM-DD HH:mm:ss'),
-            story: intent.slots.Number.value
-        });
+    const postData = JSON.stringify({
+      eventType: intent.slots.Event.value.replace(/ /g, '_'),
+      occurredAt: moment.utc().format('YYYY-MM-DD HH:mm:ss'),
+      story: intent.slots.Number.value
+    });
 
-        const options = {
-            hostname: 'fli-change.herokuapp.com',
-            port: 80,
-            path: '/events',
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'Content-Length': postData.length
-          }
-        };
+    const options = {
+      hostname: 'fli-change.herokuapp.com',
+      port: 80,
+      path: '/events',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': postData.length
+      }
+    };
 
-        const req = http.request(options, (res) => {
-          if (res.statusCode === 409) {
-            response.tell("Sorry, your event already exists.");
-          } else {
-            response.tell("Event added successfully");
-          }
-        });
+    const req = http.request(options, (res) => {
+      if (res.statusCode === 409) {
+        response.tell('Sorry, your event already exists.');
+      } else {
+        response.tell('Event added successfully');
+      }
+    });
 
-        req.on('error', (e) => {
-            response.tell("Sorry, I was unable to add your event.");
-        });
+    req.on('error', () => {
+      response.tell('Sorry, I was unable to add your event.');
+    });
 
-        req.write(postData);
-        req.end();
-    },
-    "AMAZON.HelpIntent": (intent, session, response) => {
-        response.ask("Event types are words such as start and end, and story IDs are numbers." +
-                     "A sample input is as follows: 'add event start for story 19'.");
-    }
+    req.write(postData);
+    req.end();
+  },
+  'AMAZON.HelpIntent': (intent, session, response) => {
+    response.ask('Event types are words such as start and end, and story IDs are numbers.' +
+      'A sample input is as follows: \'add event start for story 19\'.');
+  }
 };
 
 exports.handler = function (event, context) {
-    const fli = new Fli();
-    fli.execute(event, context);
+  const fli = new Fli();
+  fli.execute(event, context);
 };
