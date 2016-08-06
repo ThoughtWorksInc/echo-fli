6b870155be451b21469db36aaa4f21dfabb604ae
Addison Lee <addison.v.lee@gmail.com>
2016-08-05 17:52:19 -0700

    Attempting to reduce flakiness of e2e test as well as give a more legible error message

--- test/e2e.test.js
+++ test/e2e.test.js
@@ -7,20 +7,21 @@ const conf = require('../functions/config');
 const intentEventFactory = require('./intent-event-factory');
 
 // TODO: Create a 'delete story' route in fli so that we can start from a clean slate for each test.
-it.only('Lambda event should be persisted to FLI backend database', function (done) {
+it('Lambda event should be persisted to FLI backend database', function (done) {
 
-  this.timeout(5000);
+  this.timeout(6000);
 
   // Given
   const storyNumber = '1234567890';
   const lambda = new AWS.Lambda({region: 'us-east-1'});
   const jsonClient = restify.createJsonClient({
     url: `http://${conf.hostname}`,
-    connectTimeout: 1000,
-    requestTimeout: 1000
+    connectTimeout: 2000,
+    requestTimeout: 2000
   });
 
   jsonClient.get(`/stories/${storyNumber}`, function (err, data) {
+    assertNoError(err);
     expect(err).to.eql(null);
     const initialResponse = JSON.parse(data.res.body);
     const initialNumberOfEvents = initialResponse.story.events.length;
@@ -33,12 +34,12 @@ it.only('Lambda event should be persisted to FLI backend database', function (do
     lambda.invoke(params, function (err, data) {
 
       // Then
-      expect(err).to.eql(null);
+      assertNoError(err);
       expect(data.StatusCode).to.equal(200);
       expect(data.FunctionError).to.be.undefined;
       expect(JSON.parse(data.Payload).response.outputSpeech.text).to.equal('Event added successfully');
       jsonClient.get(`/stories/${storyNumber}`, function (err, data) {
-        expect(err).to.eql(null);
+        assertNoError(err);
         const finalResponse = JSON.parse(data.res.body);
         expect(finalResponse.story.events.length).to.equal(initialNumberOfEvents + 1);
         done();
@@ -46,4 +47,14 @@ it.only('Lambda event should be persisted to FLI backend database', function (do
     });
   });
 
+  function assertNoError(err) {
+    if (err) {
+      if (typeof err === 'string') {
+        err = JSON.parse(err);
+      }
+
+      const assert = require('chai').assert;
+      assert.fail(null, null, JSON.stringify(err, null, 2));
+    }
+  }
 });
